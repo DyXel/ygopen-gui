6c95923c160563df7a62a7125c351f088677cea2
Dylam De La Torre <dyxel04@gmail.com>
2019-07-22 05:08:28 +0200

    get rid of static_configs.h.in
    gives issues with android builds

--- src/configs.cpp
+++ src/configs.cpp
@@ -3,6 +3,19 @@
 namespace YGOpen
 {
 
+const char* CONFIG_PATH = u8"configs/";
+const char* CARD_PICTURES_PATH = u8"pics/";
+const char* CARD_DATABASES_PATH = u8"dbs/";
+const char* GAME_IMAGES_PATH = u8"assets/textures/";
+const char* GAME_SOUNDS_PATH = u8"assets/sounds/";
+
+const char* GLOBAL_CONFIG_FILENAME = u8"global.json";
+
+const char* DEFAULT_WINDOW_TITLE = u8"YGOpen";
+const int DEFAULT_WINDOW_WIDTH = 1280;
+const int DEFAULT_WINDOW_HEIGHT = 720;
+const float DEFAULT_DPI = 96.0f;
+
 static const nlohmann::json DEFAULT_GLOBAL_CONFIG_JSON =
 {
 	{"lang", "en"},
