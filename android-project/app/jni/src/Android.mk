fe588949b7f6dbbfe2ef9e4c727adf1fad956c6a
Dylam De La Torre <dyxel04@gmail.com>
2019-07-22 04:36:58 +0200

    update Android.mk to have the right flags

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -6,12 +6,14 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/..
 
 # Add your application source files here...
 LOCAL_SRC_FILES := YourSourceHere.c
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_CFLAGS := -std=c++17 -fexceptions -DFMT_HEADER_ONLY
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image protobuf
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
